b25e5a04674f57afe4c53e95af316c242dd43fc6
Zoe Carida <Zowycar@gmail.com>
2023-10-18 10:19:43 -0300

    Consumo de API

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+
+
+app.get('/', async(req, res)=> {
+    const url = 'https://k-pop.p.rapidapi.com/boy-groups?q=BTS&by=Group%20Name';
+    const options = {
+      method: 'GET',
+      headers: {
+        'X-RapidAPI-Key': 'd341cf98a3msh8326b41fd6e9090p19854cjsn92a3b603ab94',
+        'X-RapidAPI-Host': 'k-pop.p.rapidapi.com'
+      }
+    };
+    
+    try {
+        const response = await fetch(url, options);
+        const result = await response.text();
+        console.log(result);
+        res.json(result);
+    } catch (error) {
+        console.error(error);
+    }
+})
+
+app.listen(port, () =>{
+    console.log(`Example app listening on port http://localhost:${port}`)
+})
+
+
+
+/*fetch('https://k-pop.p.rapidapi.com/boy-groups?q=BTS&by=Group%20Name')
+.then(response => response.json())
+.then(data => console.log(data));
+res.send('hello word!')*/
